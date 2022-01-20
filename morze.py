528c258e89bea8e4fded7e7af1e2b1a9d3e9e6ab
victor2005-coder <65545900+victor2000-coder@users.noreply.github.com>
2022-01-20 17:17:09 +0400

    Create morze.py

---
+++ morze.py
@@ -0,0 +1,94 @@
+# Morse dict
+MorseCode = {'A': '.-',
+             'B': '-...',
+             'C': '-.-.',
+             'D': '-..',
+             'E': '.',
+             'F': '..-.',
+             'G': '--.',
+             'H': '....',
+             'I': '..',
+             'J': '.---',
+             'K': '-.-',
+             'L': '.-..',
+             'M': '--',
+             'N': '-.',
+             'O': '---',
+             'P': '.--.',
+             'Q': '--.-',
+             'R': '.-.',
+             'S': '...',
+             'T': '-',
+             'U': '..-',
+             'V': '...-',
+             'W': '.--',
+             'X': '-..-',
+             'Y': '-.--',
+             'Z': '--..',
+             ' ': '\n',
+             }
+
+
+# return encoded text
+def encode_to_morse(text: str) -> str:
+    return text
+
+
+# return decoded text
+def decode_from_morse(code: str):
+    return code
+
+
+# return True, if all letters are latin
+def check_for_encodable(text):
+    for ch in text:
+        if not ord("A") <= ord(ch) <= ord("z"):
+            return False
+    return True
+
+
+# encode and print next user input 
+def encode_input():
+    inp = input("Please, use only latin alphabet!!! I will encode this text: ")
+    while check_for_encodable(inp):
+        inp = input("\nPlease, use only latin alphabet!!! Try again: ")
+    print(encode_to_morse(inp))
+
+
+# decode and write next user input
+def decode_input():
+    inp = input("Please, use only dots and dashes, "
+                "every word must take one line, "
+                "all letters must be separated by space,"
+                "End your message by empty string."
+                "I will decode this code: ")
+    out = inp
+    while inp:
+        inp = input()
+        out += '\n' + inp
+    print(decode_from_morse(out))
+
+
+# commands
+EVENTS = {
+    'encode': encode_input,
+    'decode': decode_input,
+    'exit': exit
+}
+
+
+# return next command from input
+def get_event():
+    inp = input(f"What do you wish? {list(EVENTS.keys())}: ")
+    while inp not in EVENTS.keys():
+        inp = input(f"\nUnexcepted command: {inp}. Try again: ")
+    return EVENTS[inp]
+
+
+def main():
+    while True:
+        event = get_event()
+        event()
+
+
+main()
